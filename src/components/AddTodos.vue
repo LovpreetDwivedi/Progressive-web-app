e417b64ad8e2155c684dc2a537a04792207805e9
Lovepreet Dwivedi <lovepreet@pop-os.localdomain>
2021-02-12 10:31:09 +0530

    pwa

---
+++ src/components/AddTodos.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <h3>Add todo</h3>
+  <div>
+      <form @submit="onSubmit">
+          <input
+          type="text"
+          v-model="title"
+          placeholder="add todo...."
+          >
+          <input type="submit" value="Submit" >
+      </form>
+
+  </div>
+</div>
+</template>
+
+<script>
+import{mapActions} from 'vuex'
+export default {
+name:"AddTodos",
+data(){
+    return{
+    title:""
+    }
+},
+methods:{
+    ...mapActions(['addTodos']),
+
+    onSubmit(e){
+      e.preventDefault();
+      this.addTodos(this.title)
+    }
+}
+
+}
+</script>
+
+<style scoped>
+ form{
+     display: flex;
+ }
+ input[type="text"]{
+     flex: 10;
+     padding: 10px;
+     border:1px solid red;
+    outline: 0;
+ }
+ input[type="submit"]{
+     flex: 2;
+     background: yellow;
+     border: 1px solid red;
+     cursor: pointer;
+ }
+</style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="container">
+      <h3>todos</h3>
+      <div class="box">
+<div v-for="todo in allTodos" :key="todo.id" class="todo-box">
+         {{ todo.title}}
+         <span @click="deleteTodos(todo.id)">X</span>
+      </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import{ mapGetters ,mapActions} from 'vuex';
+export default {
+name:"Todos",
+methods: {
+    ...mapActions(['fetchTodos','deleteTodos']),
+},
+computed: mapGetters(['allTodos']),
+created(){
+    this.fetchTodos();
+
+}
+
+}
+</script>
+
+<style  scoped>
+.box{
+    display: grid;
+    grid-template-rows: repeat(3,1fr);
+    grid-gap:1rem;
+}
+
+.todo-box{
+    border: 1px solid red;
+    background: yellow;
+    padding: 10px;
+    border-radius: 5px;
+    text-align: center;
+    position: relative;
+    cursor: pointer;
+}
+span{
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+    cursor: pointer;
+}
+</style>
